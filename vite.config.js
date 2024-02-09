32dae62f423441d85f4d08cd98675b232c6bfffb
Vansh Chavda <vanshchavda111@gmail.com>
2024-02-09 12:02:01 +0530

    New Proxy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:true,
-    proxy :{
-      '/api' :"https://localhost:5173"
-    }
-  }
-})
+  
+});
